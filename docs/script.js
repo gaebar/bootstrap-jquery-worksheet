7b8a4df0d81271a9beb662120bff7d9da475a471
gaebar <gaetanobarreca@gmail.com>
2023-08-23 20:57:48 -0700

    work in progress

--- docs/script.js
+++ docs/script.js
@@ -1,51 +1,75 @@
 $(document).ready(function() {
+
+    // Change Box 1
     $("#changeBox1").click(function() {
-        // Actions for Box 1
-        console.log("Box 1 clicked!");
+        $(".bg-danger").css({
+            "background-color": "cyan",
+            "border-color": "orange"
+        });
     });
 
+    // Change Box 2
     $("#changeBox2").click(function() {
-        // Actions for Box 2
-        console.log("Box 2 clicked!");
+        $(".bg-primary").css({
+            "background-color": "magenta",
+            "border-color": "orange"
+        });
     });
 
+    // Change Box 3
     $("#changeBox3").click(function() {
-        // Actions for Box 3
-        console.log("Box 3 clicked!");
+        $(".bg-success").css({
+            "background-color": "lime",
+            "border-color": "orange"
+        });
     });
 
+    // Change All Boxes
     $("#changeAll").click(function() {
-        // Actions for all boxes
-        console.log("All boxes clicked!");
+        $(".bg-danger, .bg-primary, .bg-success").css({
+            "background-color": "gray",
+            "border-color": "orange"
+        });
     });
 
+    // Hide Box 1 (Using 'visibility' to keep space)
     $('#hideBox1').click(function() {
-        $('.bg-danger').toggle(); // toggle between show/hide for Box 1
+        $('.bg-danger').css("visibility", "hidden");
     });
 
+    // Hide Box 2 (Using 'visibility' to keep space)
     $('#hideBox2').click(function() {
-        $('.bg-primary').toggle(); // toggle between show/hide for Box 2
+        $('.bg-primary').css("visibility", "hidden");
     });
 
+    // Hide Box 3 (Using 'visibility' to keep space)
     $('#hideBox3').click(function() {
-        $('.bg-success').toggle(); // toggle between show/hide for Box 3
+        $('.bg-success').css("visibility", "hidden");
     });
 
+    // Hide All Boxes (Using 'visibility' to keep space)
     $('#hideAllBoxes').click(function() {
-        $('.bg-danger, .bg-primary, .bg-success').toggle(); // toggle between show/hide for all boxes
+        $('.bg-danger, .bg-primary, .bg-success').css("visibility", "hidden");
     });
 
+    // Reset Colors of All Boxes
     $("#resetColor").click(function() {
-    // Actions to reset the colors of the boxes
-    console.log("Reset colors!");
-    $(".bg-danger").css("background-color", "red");
-    $(".bg-primary").css("background-color", "blue");
-    $(".bg-success").css("background-color", "green");
+        $(".bg-danger").css({
+            "background-color": "red",
+            "border-color": "#000"
+        });
+        $(".bg-primary").css({
+            "background-color": "blue",
+            "border-color": "#fafa00"
+        });
+        $(".bg-success").css({
+            "background-color": "green",
+            "border-color": "#ed05ed"
+        });
     });
 
+    // Show All Boxes
     $("#showAllBoxes").click(function() {
-        // Actions to show all the boxes
-        console.log("Show all boxes!");
-        $(".bg-danger, .bg-primary, .bg-success").show();
+        $(".bg-danger, .bg-primary, .bg-success").css("visibility", "visible");
     });
 });
