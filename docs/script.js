2966311b4c6ed54968aef72f1a6bf97ae1431cd2
gaebar <gaetanobarreca@gmail.com>
2023-08-23 21:31:17 -0700

    added a random color function, plus minor changes

--- docs/script.js
+++ docs/script.js
@@ -1,68 +1,89 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 $(document).ready(function() {
 
     // Change Box 1
     $("#changeBox1").click(function() {
-        $(".bg-danger").css({
-            "background-color": "cyan",
-            "border-color": "orange"
+        let randomColor = getRandomColor();
+        $(".box1").css({
+            "background-color": randomColor,
+            "border-color": getRandomColor()
         });
     });
 
     // Change Box 2
     $("#changeBox2").click(function() {
-        $(".bg-primary").css({
-            "background-color": "magenta",
-            "border-color": "orange"
+        let randomColor = getRandomColor();
+        $(".box2").css({
+            "background-color": randomColor,
+            "border-color": getRandomColor()
         });
     });
 
     // Change Box 3
     $("#changeBox3").click(function() {
-        $(".bg-success").css({
-            "background-color": "lime",
-            "border-color": "orange"
+        let randomColor = getRandomColor();
+        $(".box3").css({
+            "background-color": randomColor,
+            "border-color": getRandomColor()
         });
     });
 
     // Change All Boxes
-    $("#changeAll").click(function() {
-        $(".bg-danger, .bg-primary, .bg-success").css({
-            "background-color": "gray",
-            "border-color": "orange"
-        });
+$("#changeAll").click(function() {
+    $(".box1").css({
+        "background-color": getRandomColor(),
+        "border-color": getRandomColor()
     });
+    $(".box2").css({
+        "background-color": getRandomColor(),
+        "border-color": getRandomColor()
+    });
+    $(".box3").css({
+        "background-color": getRandomColor(),
+        "border-color": getRandomColor()
+    });
+});
+
 
     // Hide Box 1 (Using 'visibility' to keep space)
     $('#hideBox1').click(function() {
-        $('.bg-danger').css("visibility", "hidden");
+        $('.box1').css("visibility", "hidden");
     });
 
     // Hide Box 2 (Using 'visibility' to keep space)
     $('#hideBox2').click(function() {
-        $('.bg-primary').css("visibility", "hidden");
+        $('.box2').css("visibility", "hidden");
     });
 
     // Hide Box 3 (Using 'visibility' to keep space)
     $('#hideBox3').click(function() {
-        $('.bg-success').css("visibility", "hidden");
+        $('.box3').css("visibility", "hidden");
     });
 
     // Hide All Boxes (Using 'visibility' to keep space)
     $('#hideAllBoxes').click(function() {
-        $('.bg-danger, .bg-primary, .bg-success').css("visibility", "hidden");
+        $('.box1, .box2, .box3').css("visibility", "hidden");
     });
 
     // Reset Colors of All Boxes
     $("#resetColor").click(function() {
-        $(".bg-danger").css({
+        $(".box1").css({
             "background-color": "red",
             "border-color": "#000"
         });
-        $(".bg-primary").css({
+        $(".box2").css({
             "background-color": "blue",
             "border-color": "#fafa00"
         });
-        $(".bg-success").css({
+        $(".box3").css({
             "background-color": "green",
             "border-color": "#ed05ed"
         });
@@ -70,6 +91,6 @@ $(document).ready(function() {
 
     // Show All Boxes
     $("#showAllBoxes").click(function() {
-        $(".bg-danger, .bg-primary, .bg-success").css("visibility", "visible");
+        $(".box1, .box2, .box3").css("visibility", "visible");
     });
 });
